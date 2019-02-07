6966162259805806b58bac62345e0d9c01480a7b
kdkim <kdkim@h1.com>
2019-02-07 16:24:50 +0900

    update calendar

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,9 +4,16 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println("hello");
 
+		System.out.println(
+				" 일   월   화   수   목   금  토\r\n" + 
+				"--------------------\r\n" + 
+				" 1  2  3  4  5  6  7\r\n" + 
+				" 8  9 10 11 12 13 14\r\n" + 
+				"15 16 17 18 19 20 21\r\n" + 
+				"22 23 24 25 26 27 28");
 	}
 
 }
